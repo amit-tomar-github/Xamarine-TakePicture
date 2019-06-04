b3deee559958fe5c0aaf4715c02910117cf9b12d
Amit Tomar <theamittomar@gmail.com>
2019-06-04 15:13:48 +0530

    Add project files.

---
+++ TakePicture/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Graphics;
+using Android.Provider;
+
+namespace TakePicture
+{
+    [Activity(Label = "TakePicture", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ImageView imageView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+            var btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+            btnCamera.Click += BtnCamera_Click; ;
+        }
+
+        private void BtnCamera_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)data.Extras.Get("data");
+            imageView.SetImageBitmap(bitmap);
+        }
+  
+    }
+}
+
